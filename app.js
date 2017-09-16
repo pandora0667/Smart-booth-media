ac6c31acbf0b13f0316799c9f957e1cf67a074c0
seongwonlee <swlee@wisoft.io>
2017-09-16 17:12:07 +0900

    web socket change

--- app.js
+++ app.js
@@ -1,57 +1,24 @@
-const net = require('net');
-const express = require('express'); 
+'use strict';
+
 const mplayer = require('mplayer');
-const app = express(); 
+const socket = require('socket.io-client')('http://jusk2.asuscomm.com:5002');
 let player = new mplayer();
+let count = 0;
+socket.on('connected', function (data) {
+    console.log(data);
+});
 
-app.listen(3000, function() {
-  console.log('kiosk media listeing 3000 port!!'); 
-});  
+socket.on('media', function (data) {
 
-function getConnection(connName) {
-    let client = net.connect({port: 5001, host: 'jusk2.asuscomm.com'}, function () {
-        console.log(connName + ' Connected: ');
-        console.log('   local = %s:%s', this.localAddress, this.localPort);
-        console.log('   remote = %s:%s', this.remoteAddress, this.remotePort);
-        this.setEncoding('utf8');
-        register(client);
-		
-
-        this.on('data', function (data) {
-            let re = /\0/g;
-            let str = data.toString().replace(re, "");
-            let msg = JSON.parse(str);
-			console.log(msg);
-
-			if (msg.value !== undefined) {
-				player.openFile('/home/jusk2/Videos/2.mp4');
-				player.status = {
-					volume: 100, 
-					fullscreen: true
-				}; 
-			}
-
-        });
-        this.on('end', function () {
-            console.log(connName + ' Client disconnected');
-        });
-        this.on('error', function (err) {
-            console.log('Socket Error: ', JSON.stringify(err));
-        });
-        this.on('timeout', function () {
-            console.log('Socket Timed Out');
-        });
-        this.on('close', function () {
-            console.log('Socket Closed');
-        });
-    });
-    return client;
-}
-
-
-function register(socket) {
-    let serviceRegister = {code: 'register', service: 'media'};
-    socket.write(JSON.stringify(serviceRegister));
-}
-
-const node1 = getConnection('node1');
+    if (data >= 100 && count === 1) {
+        console.log('영상을 재생합니다.');
+        player.openFile('/home/jusk2/Videos/2.mp4');
+        player.status = {
+            volume: 100,
+            fullscreen: true
+        };
+        count++;
+    } else if (data < 100 && count !==1) {
+        count = 1;
+    }
+});
